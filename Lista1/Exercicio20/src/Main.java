315943f0e669a63d962386e1c9b6394024883f9f
BrunoSakamoto <brunosakar2005@gmail.com>
2024-02-15 22:12:29 -0300

    ex20

--- Lista1/Exercicio20/src/Main.java
+++ Lista1/Exercicio20/src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         System.out.println("Escreva a raiz x1:");
-        double x1 = scanner.nextDouble();
+        double x1 = in.nextDouble();
 
         System.out.println("Escreva a raiz x2");
-        double x2 = scanner.nextDouble();
+        double x2 = in.nextDouble();
 
         double a = 1;
         double b = -(x1 + x2);
