9333bba68b8dec481e24daaaa0a8da21263c8dca
BrunoSakamoto <brunosakar2005@gmail.com>
2024-02-21 21:42:11 -0300

    blabla

---
+++ Lista2/Exercicio10/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Digite um número: ");
+        int N1 = in.nextInt();
+
+        System.out.println("Digite outro número");
+        int N2 = in.nextInt();
+
+        System.out.println("Digite outro número ");
+        int N3 = in.nextInt();
+
+
+
+    }
+}
