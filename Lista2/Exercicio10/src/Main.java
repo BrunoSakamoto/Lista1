ebe76162f0086aa0bf2b2093c1676ebdec7d80a5
BrunoSakamoto <brunosakar2005@gmail.com>
2024-02-26 18:57:09 -0300

    Ex10 e ExSwitch5

--- Lista2/Exercicio10/src/Main.java
+++ Lista2/Exercicio10/src/Main.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+
         System.out.println("Digite um número: ");
-        int N1 = in.nextInt();
-
-        System.out.println("Digite outro número");
-        int N2 = in.nextInt();
-
-        System.out.println("Digite outro número ");
-        int N3 = in.nextInt();
+        double N1 = in.nextDouble();
 
 
+        System.out.println("Digite outro número:");
+        double N2 = in.nextDouble();
+
+
+        System.out.println("Digite outro número: ");
+        double N3 = in.nextDouble();
+
+
+        double [] numeros = {N1,N2,N3};
+
+
+        Arrays.sort(numeros);
+
+
+        System.out.println("Esses números em ordem crescente são: " + Arrays.toString(numeros));
     }
 }
