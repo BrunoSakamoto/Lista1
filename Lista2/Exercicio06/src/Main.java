d1df95c2a4c550335e8e6e0b2576213bd0a77dd6
BrunoSakamoto <brunosakar2005@gmail.com>
2024-02-29 19:43:22 -0300

    Correção

--- Lista2/Exercicio06/src/Main.java
+++ Lista2/Exercicio06/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Escolha um número entre 5 e 9: ");
+        System.out.println(" 5. opção 5 \n 6. opção 6 \n 7. opção 7 \n 8. opção 8 \n 9. opção 9\n Escolha uma opção:");
         int N1 = in.nextInt();
-        System.out.println("O número escolhido foi " +N1);
+        System.out.println("A opção escolhida foi " +N1);
 
         if (N1 >9 || N1 <5)
             System.out.println("Número inválido! - O Número escolhido precisa estar entre 5 e 9. ");
--- Lista2/Exercicio10/src/Main.java
+++ Lista2/Exercicio10/src/Main.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
         System.out.println("Digite um número: ");
         double N1 = in.nextDouble();
-
 
         System.out.println("Digite outro número:");
         double N2 = in.nextDouble();
 
-
         System.out.println("Digite outro número: ");
         double N3 = in.nextDouble();
 
-
         double [] numeros = {N1,N2,N3};
 
-
         Arrays.sort(numeros);
-
 
         System.out.println("Esses números em ordem crescente são: " + Arrays.toString(numeros));
     }
